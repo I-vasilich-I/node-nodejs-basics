b167a2f84acace3c5cdd3917e74b2890071fb2c4
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-07 10:46:10 +0300

    feat: implement fs rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import { rename as fsRename } from 'fs/promises';
+import { FSError, isPathExist } from "../helpers.js";
+
 export const rename = async () => {
-    // Write your code here 
+    const WRONG_FILE_PATH = 'src/fs/files/wrongFilename.txt';
+    const PROPER_FILE_PATH = 'src/fs/files/properFilename.md';
+
+    const isWrongFileExist = await isPathExist(WRONG_FILE_PATH);
+    const isProperFileExist = await isPathExist(PROPER_FILE_PATH);
+    const shouldThrow = !isWrongFileExist || isProperFileExist;
+
+    try {
+        if (shouldThrow) {
+            throw new FSError;
+        }
+
+        await fsRename(WRONG_FILE_PATH, PROPER_FILE_PATH);
+    } catch (error) {
+        console.error(error);
+    }
 };
+
+rename();
