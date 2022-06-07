a836e44e3a6ca511528d3aac462b413725aa2fa2
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-07 12:13:06 +0300

    feat: implement cli parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,21 @@
 export const parseArgs = () => {
-    // Write your code here 
+    //I would have used minimist.
+    const args = process.argv.slice(2);
+    const res = args.reduce((acc, b, id) => {
+        let prop = b;
+        let s = ' is ';
+        if (id % 2 === 0) {
+            s = ', ';
+            prop = b.slice(2);
+        } 
+
+        if (id === 0) {
+            s = '';
+        }
+
+        return `${acc}${s}${prop}`
+    }, '');
+    console.log(res);
 };
+
+parseArgs();
