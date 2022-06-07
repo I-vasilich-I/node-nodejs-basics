3b5a2a858283a885f4bc89fb6518db8042ff2575
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-07 16:14:27 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const FILE_PATH = 'src/hash/files/fileToCalculateHashFor.txt'
+
+    const fileBuffer = await readFile(FILE_PATH);
+    const hashSum = createHash('sha256');
+    hashSum.update(fileBuffer);
+
+    const hex = hashSum.digest('hex');
+
+    console.log(hex);
 };
+
+calculateHash();
