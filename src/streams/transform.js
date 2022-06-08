1e0e8e9559c5a120c25b310f39681d8a06a05622
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-08 10:01:05 +0300

    feat: implement stream transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
+import { Transform } from "stream";
+
+const { stdin, stdout } = process;
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString('utf-8').split('').reverse().join(''));
+  },
+});
+
 export const transform = async () => {
-    // Write your code here 
+    stdin.pipe(reverse).pipe(stdout);
 };
+
+transform();
