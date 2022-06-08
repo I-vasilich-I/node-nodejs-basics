ab1b07b4c0dfab4df2461457f07e280f47f584cb
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-08 10:18:24 +0300

    feat: implement zip compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const FILE = 'src/zip/files/fileToCompress.txt';
+    const ZIP_FILE = 'src/zip/files/archive.gz';
+
+    const file = createReadStream(FILE);
+    const zip = createWriteStream(ZIP_FILE);
+
+    const stream = file.pipe(createGzip()).pipe(zip);
+
+    stream.on('finish', () => console.log('Compression is done!'));
 };
+
+compress();
